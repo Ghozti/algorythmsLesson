fea6ecdf71fe85be28b8bda80632ec06c2bb0e20
Ghozti <ybalderas1005@gmail.com>
2021-05-01 20:08:16 -0400

    - started the Array class

---
+++ src/ghozti/mousesquad/Array.java
@@ -0,0 +1,22 @@
+package ghozti.mousesquad;
+
+public class Array {
+
+    int size;
+
+    public Array(int size){
+        this.size = size;
+    }
+
+    public void insert(int i){
+
+    }
+
+    public void removeAt(int index){
+
+    }
+
+    public void print(){
+
+    }
+}
---
+++ src/ghozti/mousesquad/DataStructures.java
@@ -0,0 +1,13 @@
+package ghozti.mousesquad;
+
+public class DataStructures {
+
+    /**
+     * Arrays -
+     *
+     * they are the simplest data  structure and can be used tos tore a list of items. These items are stored sequentially in memory.
+     * The Arrays class can be used for a lot of things. Such as converting an arrau to a string. the syntax for that is:
+     * Arrays.toString(array);
+     * this is far more efficient than having to use a for loop.
+     */
+}
