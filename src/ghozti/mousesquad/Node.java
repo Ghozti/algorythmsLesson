83b990aad575adf76917d32975fb726702908646
Ghozti <ybalderas1005@gmail.com>
2021-05-04 12:38:57 -0400

    - filled in Node

--- src/ghozti/mousesquad/Node.java
+++ src/ghozti/mousesquad/Node.java
@@ -3,4 +3,16 @@ package ghozti.mousesquad;
 public class Node {
     private int value;
     private Node next;//reference for next node
+
+    public Node(int val){
+        this.value = val;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public Node getNext(){
+        return next;
+    }
 }
